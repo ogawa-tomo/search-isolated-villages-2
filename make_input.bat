5f50b20eebe730d94a90e7cb29e6f3c2bbff87d6
unknown <o_tomo_1990@yahoo.co.jp>
2019-11-17 21:56:43 +0900

    batファイルとドキュメントの編集

--- make_input.bat
+++ make_input.bat
@@ -3,3 +3,18 @@ python download_raw_data.py
 python make_input_main_1.py
 pypy3 make_input_main_2.py
 pypy3 make_input_main_3.py
+
+python make_input_faculty_main_1.py elementary_school
+pypy3 make_input_faculty_main_2.py elementary_school
+
+python make_input_faculty_main_1.py post_office
+pypy3 make_input_faculty_main_2.py post_office
+
+python make_input_faculty_main_1.py new_town
+pypy3 make_input_faculty_main_2.py new_town
+
+python make_input_faculty_main_1.py michinoeki
+pypy3 make_input_faculty_main_2.py michinoeki
+
+python make_input_faculty_main_1.py station
+pypy3 make_input_faculty_main_2.py station
