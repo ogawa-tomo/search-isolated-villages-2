8dec5a496e9556064602031ae1e8229074b7b24c
unknown <o_tomo_1990@yahoo.co.jp>
2020-05-02 14:12:37 +0900

    r774データ更新r774データ更新r774データ更新r774データ更新r774データ更新r774データ更新r774データ更新

---
+++ make_input_r774.bat
@@ -0,0 +1,17 @@
+
+pypy3 make_input_main_5.py
+python make_input_main_6.py
+
+python make_input_faculty_main_3.py elementary_school
+
+python make_input_faculty_main_3.py post_office
+
+python make_input_faculty_main_3.py new_town
+
+python make_input_faculty_main_3.py michinoeki
+
+python make_input_faculty_main_3.py station
+
+python make_input_faculty_main_3.py abandoned_station
+
+python make_input_faculty_main_3.py research_institute
