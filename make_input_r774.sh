96416795d84647e4d4ffed761ee625838b2344b2
tomo <ec2-user@ip-172-31-54-220.ec2.internal>
2021-01-03 10:34:23 +0000

    シェルスクリプトの更新

--- make_input_r774.sh
+++ make_input_r774.sh
@@ -5,5 +5,6 @@ pipenv run python make_input_main_6.py
 
 for faculty in elementary_school post_office new_town michinoeki station abandoned_station research_institute
 do
+echo $faculty
 pipenv run python make_input_faculty_main_3.py $faculty
 done
