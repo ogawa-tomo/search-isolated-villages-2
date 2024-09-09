5a8cb4ca806679545cad13487bab41856da4384a
tomo <o_tomo_1990@yahoo.co.jp>
2024-09-09 20:59:03 +0900

    APIのパラメータを英語にする

--- webif.py
+++ webif.py
@@ -72,12 +72,12 @@ def post():
 def api_result():
 
     year = 2020 # request.form["year"]
-    region = request.args.get("region")
+    region = convert_region_name(request.args.get("region"))
     village_pop_lower_limit = int(request.args.get("populationLowerLimit"))
     village_pop_upper_limit = int(request.args.get("populationUpperLimit"))
     village_size_lower_limit = 0 # int(request.form["village_size_lower_limit"])
     village_size_upper_limit = 100 # int(request.form["village_size_upper_limit"])
-    island_setting = request.args.get("islandSetting")
+    island_setting = convert_island_setting(request.args.get("islandSetting"))
     key_words = request.args.get("keywords")
     page = int(request.args.get("page"))
 
@@ -113,8 +113,8 @@ def api_result():
 @app.route("/api/<faculty>/result", methods=["GET"])
 def api_faculty_result(faculty):
     year = 2020 # request.form["year"]
-    region = request.args.get("region")
-    island_setting = request.args.get("islandSetting")
+    region = convert_region_name(request.args.get("region"))
+    island_setting = convert_island_setting(request.args.get("islandSetting"))
     key_words = request.args.get("keywords")
     page = int(request.args.get("page"))
 
@@ -230,6 +230,75 @@ def about():
     :return:
     """
     return render_template("about.html")
+
+def convert_region_name(region_en_name):
+    region_dict = {
+        "all_country": "全国",
+        "hokkaido": "北海道",
+        "tohoku": "東北",
+        "kanto": "関東",
+        "hokuriku": "北陸",
+        "chubu": "中部",
+        "kinki": "近畿",
+        "chugoku": "中国",
+        "shikoku": "四国",
+        "kyushu": "九州",
+        "okinawa": "沖縄",
+        "aomori": "青森県",
+        "iwate": "岩手県",
+        "miyagi": "宮城県",
+        "akita": "秋田県",
+        "yamagata": "山形県",
+        "fukushima": "福島県",
+        "ibaraki": "茨城県",
+        "tochigi": "栃木県",
+        "gunma": "群馬県",
+        "saitama": "埼玉県",
+        "chiba": "千葉県",
+        "tokyo": "東京都",
+        "kanagawa": "神奈川県",
+        "niigata": "新潟県",
+        "toyama": "富山県",
+        "ishikawa": "石川県",
+        "fukui": "福井県",
+        "yamanashi": "山梨県",
+        "nagano": "長野県",
+        "gifu": "岐阜県",
+        "shizuoka": "静岡県",
+        "aichi": "愛知県",
+        "mie": "三重県",
+        "shiga": "滋賀県",
+        "kyoto": "京都府",
+        "osaka": "大阪府",
+        "hyogo": "兵庫県",
+        "nara": "奈良県",
+        "wakayama": "和歌山県",
+        "tottori": "鳥取県",
+        "shimane": "島根県",
+        "okayama": "岡山県",
+        "hiroshima": "広島県",
+        "yamaguchi": "山口県",
+        "tokushima": "徳島県",
+        "kagawa": "香川県",
+        "ehime": "愛媛県",
+        "kochi": "高知県",
+        "fukuoka": "福岡県",
+        "saga": "佐賀県",
+        "nagasaki": "長崎県",
+        "kumamoto": "熊本県",
+        "oita": "大分県",
+        "miyazaki": "宮崎県",
+        "kagoshima": "鹿児島県"
+    }
+    return region_dict[region_en_name]
+
+def convert_island_setting(island_setting_en):
+    if island_setting_en == "exclude_islands":
+        return EXCLUDE_ISLANDS
+    elif island_setting_en == "include_islands":
+        return INCLUDE_ISLANDS
+    elif island_setting_en == "only_islands":
+        return ONLY_ISLANDS
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
